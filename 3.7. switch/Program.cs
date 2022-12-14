bf26ac4c29563baebe61e812e2300b861d76be8a
Yulia Miller <miller82.98@mail.ru>
2022-12-13 23:09:19 -0800

    Добавьте файлы проекта.

---
+++ 3.7. switch/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace mynamespase
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Введите свой любимый цвет на английском, с маленькой буквы");
+
+            var color = Console.ReadLine();
+
+            switch (color)
+            {
+                case "red":
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    Console.ForegroundColor = ConsoleColor.Black;
+
+                    Console.WriteLine("Your color is red!");
+                    break;
+                case "green":
+                    Console.BackgroundColor = ConsoleColor.Green;
+                    Console.ForegroundColor = ConsoleColor.Black;
+
+                    Console.WriteLine("Your color is green!");
+                    break;
+                default:
+                Console.BackgroundColor = ConsoleColor.Cyan;
+                Console.ForegroundColor = ConsoleColor.Black;
+
+                Console.WriteLine("Your color is cyan!");
+                    break;
+            }
+        }
+    }
+}
